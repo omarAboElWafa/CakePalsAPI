220fb7c67c0943eb967ff3715114322d2f268360
Omar A. H. Abo El Wafa <58788836+omarAboElWafa@users.noreply.github.com>
2023-09-24 07:18:26 +0300

    edit rating entity

--- src/components/rating/rating.entities.ts
+++ src/components/rating/rating.entities.ts
@@ -1,7 +1,25 @@
 import { IRating } from "@/contracts/rating";
 import mongoose, { Schema } from "mongoose";
 
-const  RatingSchema : Schema  = new Schema({});
+const  RatingSchema : Schema  = new Schema({
+    bakeryId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Bakery',
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    rating: {
+        type: Number,
+        required: true,
+    },
+    comment: {
+        type: String,
+        required: false,
+    }
+
+});
 
 
 
