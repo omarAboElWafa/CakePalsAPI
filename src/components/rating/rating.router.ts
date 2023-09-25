a41ec01e27ccdc191ac2b73ff16488acba03d09e
Omar A. H. Abo El Wafa <58788836+omarAboElWafa@users.noreply.github.com>
2023-09-25 22:58:30 +0300

    final edits for the PoC version of CakePal API

--- src/components/rating/rating.router.ts
+++ src/components/rating/rating.router.ts
@@ -10,7 +10,6 @@ class RatingRouter {
     getRouter = () => {
         const router = Router();
         router.post('/create', this.ratingsController.create);
-        // TODO: get all my ratings
         router.get('/get', this.ratingsController.get);
         router.get('/get/:id', this.ratingsController.getById);
         router.put('/update/:id', this.ratingsController.update);
--- src/loaders/routes.ts
+++ src/loaders/routes.ts
@@ -1,7 +1,13 @@
 import { Express } from 'express';
 import userModule from '../components/user/user.module';
+import productModule from '../components/product/product.module';
+import orderModule from '../components/order/order.module';
+import ratingModule from '../components/rating/rating.module';
 
 
 export default (app : Express) => {
     app.use('/users', userModule.router);
+    app.use('/products', productModule.router);
+    app.use('/orders', orderModule.router);
+    app.use('/ratings', ratingModule.router);
 }
