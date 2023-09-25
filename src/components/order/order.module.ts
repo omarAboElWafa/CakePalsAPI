87aca0bf27aff74a056380784d804007561582d4
Omar A. H. Abo El Wafa <58788836+omarAboElWafa@users.noreply.github.com>
2023-09-25 10:55:29 +0300

    invoking (product, order, and rating) modules

--- src/components/order/order.module.ts
+++ src/components/order/order.module.ts
@@ -1,3 +1,13 @@
 import OrderController from './order.controller';
 import OrderService from './order.service';
 import OrderRouter from './order.router';
+
+const orderService = new OrderService();
+const orderController = new OrderController(orderService);
+const orderRouter = new OrderRouter(orderController);
+
+export default {
+    service: orderService,
+    controller: orderController,
+    router: orderRouter.getRouter()
+}
--- src/components/rating/rating.module.ts
+++ src/components/rating/rating.module.ts
@@ -1,3 +1,13 @@
 import RatingController from './rating.controller';
 import RatingService from './rating.service';
 import RatingRouter from './rating.router';
+
+const ratingService = new RatingService();
+const ratingController = new RatingController(ratingService);
+const ratingRouter = new RatingRouter(ratingController);
+
+export default {
+    service: ratingService,
+    controller: ratingController,
+    router: ratingRouter.getRouter()
+}
